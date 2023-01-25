3c6a892c81305e27fe9c1986f98462950735ad5b
Harsh Suthar <sutharharsh99@gmail.com>
2023-01-25 11:22:36 -0500

    Added helpers class and file readers

--- src/main/java/org/SeleniumFramework/Config/ConfigReader.java
+++ src/main/java/org/SeleniumFramework/Config/ConfigReader.java
@@ -1,1 +1,32 @@
 package org.SeleniumFramework.Config;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private Properties prop = new Properties();
+    private InputStream input = null;
+
+    public ConfigReader() {
+        try {
+            input = new FileInputStream("src/main/java/org/SeleniumFramework/config.properties");
+            prop.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+}
--- src/main/java/org/SeleniumFramework/Helpers/AssertionHelper.java
+++ src/main/java/org/SeleniumFramework/Helpers/AssertionHelper.java
@@ -1,1 +1,28 @@
 package org.SeleniumFramework.Helpers;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class AssertionHelper {
+    private final WebDriver driver;
+
+    public AssertionHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyPageTitle(String expectedTitle) {
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    public void verifyText(WebElement locator, String expectedText) {
+        String actualText = locator.getText();
+        Assert.assertEquals(actualText, expectedText);
+    }
+
+    public void verifyElementPresent(WebElement locator) {
+        boolean isDisplayed = locator.isDisplayed();
+        Assert.assertTrue(isDisplayed);
+    }
+
+}
